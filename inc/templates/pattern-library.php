629e86275cc7bab303139121979b423dc292a739
John Blackbourn <johnbillion@gmail.com>
2019-02-25 15:55:36 +0530

    Introduce a template for displaying the template parts.

---
+++ inc/templates/pattern-library.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template to display the pattern library.
+ *
+ * @package Kalutara
+ */
+
+namespace Kalutara;
+
+$directories = [];
+
+if ( is_child_theme() ) {
+	$directories[] = get_stylesheet_directory() . '/template-parts';
+}
+
+$directories[] = get_template_directory() . '/template-parts';
+
+\get_header();
+
+foreach ( $directories as $directory ) :
+	foreach ( Helpers\get_files_in_path( $directory ) as $file ) : ?>
+		<article class="kalutara-component <?php echo esc_attr( Helpers\get_css_class_name( $file ) ); ?>">
+			<h2><?php echo esc_html( $file ); ?></h2>
+			<?php
+			get_extended_template_part(
+				Helpers\remove_extension_from_filename( $file ),
+				null,
+				Data\get_dummy_data( $file )
+			);
+			?>
+		</article>
+		<?php
+	endforeach;
+endforeach;
+
+\get_footer();
