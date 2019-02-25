35be2b44ef085d2e1784e0fc0cc3e487837b59b4
John Blackbourn <johnbillion@gmail.com>
2019-02-25 16:17:34 +0530

    Update dependencies.

--- plugin.php
+++ plugin.php
@@ -14,7 +14,7 @@
  * Author URI: https://humanmade.com
  * Text Domain: hm-kalutara
  * Domain Path: /languages
- * Requires PHP: 5.3
+ * Requires PHP: 7.0
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
