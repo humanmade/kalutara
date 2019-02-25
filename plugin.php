fa9322aec9028be5b049ab57927829fe6a612431
John Blackbourn <johnbillion@gmail.com>
2019-02-25 15:40:38 +0530

    Plugin header updates.

--- plugin.php
+++ plugin.php
@@ -1,11 +1,28 @@
 <?php
 /**
+ * Kalutara Pattern Library plugin for WordPress
+ *
+ * @package   kalutara
+ * @copyright 2019 Human Made
+ * @license   GPL v2 or later
+ *
  * Plugin Name: Kalutara
  * Plugin URI: https://github.com/humanmade/kalutara
- * Description: A pattern library plugin for WordPress themes./
+ * Description: A pattern library plugin for WordPress themes.
  * Version: 0.1.0
  * Author: Human Made
  * Author URI: https://humanmade.com
  * Text Domain: hm-kalutara
  * Domain Path: /languages
+ * Requires PHP: 5.3
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
