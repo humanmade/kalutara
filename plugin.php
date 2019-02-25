20eccca2dc58a88f280e09bb4c5a1a6ea6f04fdc
Matthew Haines-Young <matthew@matth.eu>
2019-02-25 15:05:39 +0530

    Coding standards

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Kalutara
+ * Plugin URI: https://github.com/humanmade/kalutara
+ * Description: A pattern library plugin for WordPress themes./
+ * Version: 0.1.0
+ * Author: Human Made
+ * Author URI: https://humanmade.com
+ * Text Domain: hm-kalutara
+ * Domain Path: /languages
+ */
