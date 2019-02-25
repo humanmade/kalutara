6f8ed5733c1f5c2f7df08fe12a9ed83957a73c61
sambulance <sam@hmn.md>
2019-02-25 16:54:46 +0530

    Add a basic HTML layout for the pattern library template

--- inc/templates/pattern-library.php
+++ inc/templates/pattern-library.php
@@ -8,18 +8,25 @@
 namespace Kalutara;
 
 $directories = [];
-
 if ( is_child_theme() ) {
 	$directories[] = get_stylesheet_directory() . '/template-parts';
 }
-
 $directories[] = get_template_directory() . '/template-parts';
+
+$files = [];
+foreach ( $directories as $directory ) {
+	$files = Helpers\get_files_in_path( $directory );
+}
 
 \get_header();
 
-foreach ( $directories as $directory ) :
-	foreach ( Helpers\get_files_in_path( $directory ) as $file ) : ?>
-		<article class="kalutara-component <?php echo esc_attr( Helpers\get_css_class_name( $file ) ); ?>">
+?>
+<main class="kalutara-viewport">
+	<?php foreach ( $files as $file ) : ?>
+		<article
+			class="kalutara-component <?php echo esc_attr( Helpers\get_css_class_name( $file ) ); ?>"
+			id="<?php echo esc_attr( Helpers\get_css_class_name( $file ) ); ?>"
+		>
 			<h2><?php echo esc_html( $file ); ?></h2>
 			<?php
 			get_extended_template_part(
@@ -29,8 +36,25 @@ foreach ( $directories as $directory ) :
 			);
 			?>
 		</article>
-		<?php
-	endforeach;
-endforeach;
+	<?php endforeach; ?>
+</main>
+
+<nav class="kalutara-sidebar">
+	<ul>
+		<?php foreach ( $files as $file ) : ?>
+			<li class="kalutara-sidebar__item">
+				<a href="#<?php echo esc_attr( Helpers\get_css_class_name( $file ) ); ?>" class="kalutara-sidebar__link">
+					<?php echo esc_html( $file ); ?>
+				</a>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+</nav>
+
+<aside class="kalutara-docbox">
+	<em>Documentation will live here</em>
+</aside>
+
+<?php
 
 \get_footer();
