de1f757ee217b06fd11c2c3b00468e69d14bb983
goldenapples <goldenapplesdesign@gmail.com>
2019-02-26 08:32:30 +0530

    Pass data into pattern library component
    Takes the parsed docblock, outputs the summary and description in the
    pattern library block, and parses the data values to pass into the block
    rendering function.

--- inc/parser.php
+++ inc/parser.php
@@ -30,12 +30,55 @@ function get_template_part_header( $file ) {
 	$parsed = $reflector_factory->create( $matches[0] );
 
 	return [
-		'name'           => $parsed->getSummary(),
-		'documentation'  => $parsed->getDescription(),
+		'summary'        => $parsed->getSummary(),
+		'documentation'  => $parsed->getDescription()->render(),
 		'template_vars'  => $parsed->getTagsByName( 'var', null, true ),
 		'globals'        => $parsed->getTagsByName( 'global', null, true ),
-		'data'           => $parsed->getTagsByName( 'data', null, true ),
-		'data_providers' => $parsed->getTagsByName( 'dataProviders', null, true ),
+		'data'           => parse_data(
+			$parsed->getTagsByName( 'data', null, true )
+		),
+		'data_providers' => parse_data_providers(
+			$parsed->getTagsByName( 'dataProviders', null, true )
+		),
 	];
 }
 
+/**
+ * Parse the json data from a @data attribute.
+ *
+ * @param DocBlock\Tag[] $data Any 'data' entries in the docblock.
+ * @return [] Array of decoded data values.
+ */
+function parse_data( $data ) {
+	return array_map(
+		function ( $data_entry ) {
+			$data_value = $data_entry->getDescription()->render();
+			return json_decode( $data_value );
+		},
+		$data
+	);
+}
+
+/**
+ * Parse the json data providers from a @data attribute.
+ *
+ * If any data providers are found on the attribute, calls those functions and
+ * returns the result.
+ *
+ * @param DocBlock\Tag[] $data_providers Any 'data_provider' entries in the docblock.
+ * @return [] Array of decoded data values.
+ */
+function parse_data_providers( $data_providers ) {
+	$data_provider_values = array_map(
+		function ( $data_provider_entry ) {
+			$data_provider_func = $data_provider_entry->getDescription()->render();
+
+			return is_callable( $data_provider_func ) ?
+				call_user_func( $data_provider_func ) :
+				[];
+		},
+		$data_provider
+	);
+
+	return array_filter( $data_providers );
+}
--- inc/templates/pattern-library.php
+++ inc/templates/pattern-library.php
@@ -18,16 +18,43 @@ $directories[] = get_template_directory() . '/template-parts';
 \get_header();
 
 foreach ( $directories as $directory ) :
-	foreach ( Helpers\get_files_in_path( $directory ) as $file ) : ?>
+	foreach ( Helpers\get_files_in_path( $directory ) as $file ) :
+
+		if ( 'post-card.php' !== $file ) continue;
+
+		$file_path = trailingslashit( $directory ) . $file;
+		$file_documentation = Parser\get_template_part_header( $file_path );
+		?>
 		<article class="kalutara-component <?php echo esc_attr( Helpers\get_css_class_name( $file ) ); ?>">
-			<h2><?php echo esc_html( $file ); ?></h2>
+			<strong><?php echo esc_html( $file ); ?></strong>
 			<?php
-			get_extended_template_part(
-				Helpers\remove_extension_from_filename( $file ),
-				'',
-				Data\get_dummy_data( $file )
-			);
+
+			if ( ! empty( $file_documentation['summary'] ) ) :
+				?>
+				<h3 class="kalutara-component__summary">
+					<?php echo esc_html( $file_documentation['summary'] ); ?>
+				</h3>
+				<?php
+			endif;
+
+			if ( ! empty( $file_documentation['documentation'] ) ) :
+				?>
+				<p class="kalutara-component__documentation">
+					<?php echo esc_html( $file_documentation['documentation'] ); ?>
+				</p>
+				<?php
+			endif;
 			?>
+
+			<div class="kalutara-component__preview">
+				<?php
+				get_extended_template_part(
+					Helpers\remove_extension_from_filename( $file ),
+					'',
+					Data\get_data( $file_path )
+				);
+				?>
+			</div>
 		</article>
 		<?php
 	endforeach;
